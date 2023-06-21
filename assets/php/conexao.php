388d67a27d9a1838940a8bbcd32a3aea893ed79d
AnDraDeDS <andradevieira909@gmail.com>
2023-06-21 11:07:01 -0300

    'Conexao com mysql'

--- assets/php/conexao.php
+++ assets/php/conexao.php
@@ -4,4 +4,9 @@ $user = 'root';
 $database = 'academia';
 $password = '';
 
+$connection = new mysqli($host, $user, $password, $database);
+
+if($connection->connect_error){
+    die('Falha de conexao: '. $connection->connect_error);
+}
 ?>
