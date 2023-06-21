a46f7daec1992d9fa1e2f15560f74b6aa28ef7f0
AnDraDeDS <andradevieira909@gmail.com>
2023-06-21 11:06:15 -0300

    Estrutura do banco

--- assets/sql/academia.sql
+++ assets/sql/academia.sql
@@ -1,18 +1,19 @@
+-- Active: 1687356205749@@127.0.0.1@3306
 CREATE DATABASE IF NOT EXISTS academia;
 USE academia;
 
 CREATE TABLE cliente (
 codigo int PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(100),
-telefone INT(13)
+telefone INT(13),
 dataCadastro DATE
 );
 
-CREATE TABLE pagamaneto (
-codigo INT PRIMARY KEY AUTO_INCREMENT
+CREATE TABLE pagamento (
+codigo INT PRIMARY KEY AUTO_INCREMENT,
 codigoCliente INT,
 nomeCliente VARCHAR(100),
 dataPagamento DATE,
-valorPagamento DECIMAL(5,2)
-FOREIGN KEY (codigoCliente) REFERENCES cliente(codigo),
+valorPagamento DECIMAL(5,2),
+FOREIGN KEY (codigoCliente) REFERENCES cliente(codigo)
 );
