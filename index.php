e690334b7ac5d2d504e410902b177c0805bc602b
Paiva6002 <paivag1109@gmail.com>
2023-06-25 12:07:56 -0300

    Alteração de classes

--- index.php
+++ index.php
@@ -2,36 +2,33 @@
 <html lang="pt-br">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Academia</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="shortcut icon" href="./assets/images/logo-site.ico" type="image/x-icon" />
-    <link rel="stylesheet" href="./assets/css/style.css">
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>Academia</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link rel="shortcut icon" href="./assets/images/logo-site.ico" type="image/x-icon" />
+  <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
 <body>
-    <div class="container-fluid">
-        <div class="flex-container d-flex">
-            <div class="flex-item item1 align-self-start"><a href="./index.php">Home</a></div>
-            <div class="flex-item item2"><a href="./cadastrar.php">Cadastrar</a></div>
-          </div>
-        <div class="row d-flex justify-content-center align-items-center">
-            <div class="col-1 ">
-                <div class="container-slogan">
-                    <h1 class="text-center mt-5">"Quem cresce natural é planta"</h1>
-                    <br>
-                    <img src="./assets/images/logo.svg" class="left-side__brand   img-fluig" height="400px"
-                        width="400px" />
-                </div>
-
-            </div>
+  <div class="header d-flex">
+    <div class="flex-item item1 align-self-start"><a href="./index.php">Home</a></div>
+    <div class="flex-item item2"><a href="./cadastrar.php">Cadastrar</a></div>
+  </div>
+  <div class="container-fluid">
+    <div class="row d-flex justify-content-center align-items-center">
+      <div class="col-6 d-flex flex-column align-items-center">
+          <h1 class="text-center mt-5">"Quem cresce natural é planta"</h1>
+          <img src="./assets/images/logo.svg" class="left-side__brand   img-fluig" height="400px" width="400px" />
         </div>
+
+      </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
-        crossorigin="anonymous"></script>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+    crossorigin="anonymous"></script>
 </body>
 
 
